d9712bbcf1413a2f3a6ec864a42aed29bb9fb0ed
JuanLu1411 <94109552+JuanLu1411@users.noreply.github.com>
2022-01-26 18:06:18 +0100

    Add files via upload

--- src/AsignarEquipo.java
+++ src/AsignarEquipo.java
@@ -1,12 +1,6 @@
-
 public class AsignarEquipo {
-<<<<<<< HEAD
       private Equipo equipo;
       private Jugador jugador;
-=======
-      private static Equipo equipo;
-      private static Jugador jugador;
->>>>>>> c63ca70797e887e7daa361ac6e475c8396732d0e
       
       
       public AsignarEquipo() {
@@ -15,15 +9,10 @@ public class AsignarEquipo {
       
       public void setEquipo(Equipo equipo) {
   		//code here
-<<<<<<< HEAD
-=======
-    	  this.equipo = equipo;
->>>>>>> c63ca70797e887e7daa361ac6e475c8396732d0e
       }
       
       public void setJugador(Jugador jugador) {
   		//code here
-<<<<<<< HEAD
       }
       
       public Equipo getEquipo(){
@@ -32,17 +21,6 @@ public class AsignarEquipo {
       
       public Jugador getJugador(){
          return jugador;
-=======
-    	  this.jugador=jugador;
-      }
-      
-      public  Equipo getEquipo(){
-          return this.equipo;
-      }
-      
-      public  Jugador getJugador(){
-         return this.jugador;
->>>>>>> c63ca70797e887e7daa361ac6e475c8396732d0e
       }
       
 }
