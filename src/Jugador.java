1398b92c12085b6597923960154dbd0106923479
DawFC <manuel.s254881@cesurformacion.com>
2022-02-03 11:21:15 +0100

    codigo jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -10,13 +10,40 @@ public class Jugador {
 	
 	public void setNombreJugador(String nombre) {
 		//code here
+		if (nombre.length()<4 && nombre.length()>20) {
+			for (int i=0; i <nombre.length();i++) {
+				if (!Character.isLetter(nombre.charAt(i))){
+					this.nombre=null;
+				}else {
+					this.nombre=nombre;
+				}
+			}
+		}else{
+			this.nombre= null;
+		}
 	}
 	
 	public void setEdad(int edad) {
-		//code here
+		
+		if (edad>18) {
+			
+			this.edad=edad;
+
+		}else{
+			
+			this.edad= -1;
+		}
 	}
 	public void setIdioma(String idioma) {
-		//code here
+		String idiomaM = "";
+		for (int i=0;i<idioma.length();i++) {
+			idiomaM+= Character.toUpperCase(idioma.charAt(i));
+		}
+		if(idiomaM == "INGLES" || idiomaM == "ESPANOL" || idiomaM == "FRANCES" || idiomaM == "ALEMAN") {
+			this.idioma=idioma;
+		}else {
+			this.idioma=null;
+		}
 	}
 	
 
