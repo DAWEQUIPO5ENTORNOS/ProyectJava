1603bba2d807da7e98cfd680d3c7d7dac27aabdf
JuanLu1411 <94109552+JuanLu1411@users.noreply.github.com>
2022-02-04 09:01:24 +0100

    Update Equipo.java

--- src/Equipo.java
+++ src/Equipo.java
@@ -2,19 +2,47 @@
 public class Equipo {
 	private String nombreEquipo;
 	private int ranking;
-	
-	
+
+
 	public Equipo () {
 	}
-	
-	
+
+
 	public void setNombreEquipo(String nombreEquipo) {
-		//code here
+		if (nombreEquipo.length()<4 && nombreEquipo.length()>20) {
+			for (int i=0; i <nombreEquipo.length();i++) {
+				if (!Character.isLetter(nombreEquipo.charAt(i))){
+					this.nombreEquipo=null;
+				}else {
+					this.nombreEquipo=nombreEquipo;
+				}
+			}
+		}else{
+			this.nombreEquipo= null;
+		}
 	}
 	public void setRanking(int ranking) {
-		//code here
+		String R1 = "Primera";
+		String R2 = "Segunda";
+		String R3 = "Tercera";
+
+		if(ranking > 10) {
+			this.ranking = -1;
+		}
+		if(ranking <= 10 && ranking >= 7) {
+			R1 = "primera";
+			this.ranking = ranking;
+		}else if(ranking >= 3 && ranking <= 6) {
+			R2 = "Segunda";
+			this.ranking = ranking;
+		}else if(ranking >= 0 && ranking <= 3) {
+			R3 = "Tercera";
+			this.ranking = ranking;
+		}else if(ranking < 0);{
+			this.ranking = -1;
+		}
 	}
-	
+
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
