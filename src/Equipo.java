dbfe9001863df4bab0e67ced38c174b35565846e
rodapie <miguels.266401@cesurformacion.com>
2022-02-04 12:22:16 +0100

    Update Equipo.java

--- src/Equipo.java
+++ src/Equipo.java
@@ -9,37 +9,40 @@ public class Equipo {
 
 
 	public void setNombreEquipo(String nombreEquipo) {
-		if (nombreEquipo.length()<4 && nombreEquipo.length()>20) {
-			for (int i=0; i <nombreEquipo.length();i++) {
-				if (!Character.isLetter(nombreEquipo.charAt(i))){
-					this.nombreEquipo=null;
-				}else {
-					this.nombreEquipo=nombreEquipo;
+		if (nombreEquipo != null){
+			if (nombreEquipo.length()<4 && nombreEquipo.length()>20) {
+				for (int i=0; i <nombreEquipo.length();i++) {
+					if (nombreEquipo.matches("[a-zA-Z]+")){
+						this.nombreEquipo=nombreEquipo;
+					}else {
+						this.nombreEquipo=null;
+					}
 				}
+			}else{
+				this.nombreEquipo= null;
 			}
 		}else{
 			this.nombreEquipo= null;
 		}
 	}
 	public void setRanking(int ranking) {
-		String R1 = "Primera";
-		String R2 = "Segunda";
-		String R3 = "Tercera";
+		this.ranking = -1;
+		if(ranking >= 0 && ranking <=10){
+			this.ranking = ranking;
+		}
+	}
 
+	public String categoriaEquipo(int ranking){
 		if(ranking <= 10 && ranking >= 7) {
-			R1 = "primera";
-			this.ranking = ranking;
-		}else if(ranking >= 3 && ranking <= 6) {
-			R2 = "Segunda";
-			this.ranking = ranking;
+			this.ranking = "primera";
+		}else if(ranking > 3 && ranking <= 6) {
+			this.ranking ="Segunda";
 		}else if(ranking >= 0 && ranking <= 3) {
-			R3 = "Tercera";
-			this.ranking = ranking;
+			this.ranking = "Tercera";
 		}else{
 			this.ranking = -1;
 		}
 	}
-
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
